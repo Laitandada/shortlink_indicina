84579f79bf2666ddfb0345c560f50a4551bd68f6
Olaitan Dada <Michaeldada09@gmail.com>
2024-03-25 23:45:11 +0100

    created additional logic and endpoints

--- server/src/controllers/urlController.ts
+++ server/src/controllers/urlController.ts
@@ -1,7 +1,7 @@
 import express from "express";
 import { urlModel } from "../models/urlSchema";
 
-
+// logic for encodiq url
 export const encodeUrl = async (
   req: express.Request,
   res: express.Response
@@ -12,7 +12,7 @@ export const encodeUrl = async (
     const urlFound = await urlModel.find({ decodedUrl });
     if (urlFound.length > 0) {
       res.status(409);
-      res.send(urlFound);
+      res.send({ message: "Url already encoded", foundUrl: urlFound });
     } else {
       const encodedUrl = await urlModel.create({ decodedUrl });
       res.status(201).send({ message: "Url encoded successfully", encodedUrl: encodedUrl });
@@ -22,20 +22,77 @@ export const encodeUrl = async (
   }
 };
 
+// logic for decoding url
 export const decodeUrl = async (
-  req: express.Request,
-  res: express.Response
-) => {
-  try {
-    const { encodedUrl } = req.body;
-    const decodedUrl = await urlModel.findOne({encodedUrl})
-    if (decodedUrl) {
-      res.status(404).send({ message: "Encoded Url not found" });
-    } else {
-      res.status(200).send({ message: "Url decoded successfully", decodedUrl: decodedUrl!.decodedUrl });
+    req: express.Request,
+    res: express.Response
+  ) => {
+    try {
+      const { encodedUrl } = req.body;
+      console.log(encodedUrl);
+      const decodedUrl = await urlModel.findOne({ encodedUrl: encodedUrl });
+      if (!decodedUrl) {
+        res.status(404).send({ message: "Encoded URL not found" });
+      } else {
+       
+        const decodedUrlValue = decodedUrl.decodedUrl;
+        res.status(200).send({ message: "URL decoded successfully", decodedUrl: decodedUrlValue });
+      }
+    } catch (error) {
+      res.status(500).send({ message: "Something went wrong!" });
     }
-  } catch (error) {
-    res.status(500).send({ message: "Something went wrong!" });
-  }
-};
+  };
+  
+//   logic for getting encoded URL statistics
+  export const getUrlStats = async (
+    req: express.Request,
+    res: express.Response
+  ) => {
+    try {
+        const shortUrlStats = await urlModel.findOne({ encodedUrl: req.params.encodedUrl });
+        if (!shortUrlStats) {
+          res.status(404).send({ message: "Url not found!" });
+        } else {
+            res.status(200).send({ message: "URL found successfully", shortUrlStats: shortUrlStats });
+        }
+      } catch (error) {
+        res.status(500).send({ message: "Something went wrong!" });
+      }
+  };
 
+
+  export const getAllEncodedUrls = async (
+    req: express.Request,
+    res: express.Response
+  ) => {
+    try {
+      const allEncodedUrls = await urlModel.find().sort({ createdAt: -1 });
+      if (allEncodedUrls.length < 0) {
+        res.status(404).send({ message: "EncodedUrls not found!" });
+      } else {
+        res.status(200).send({ message: "All EncodedUrls found successfully", allEncodedUrls: allEncodedUrls  });
+      }
+    } catch (error) {
+      res.status(500).send({ message: "Something went wrong!" });
+    }
+  };
+  
+  export const goToEncodedUrlLink = async (req: express.Request, res: express.Response) => {
+    try {
+      const encodedUrl = await urlModel.findOne({ encodedUrl: req.params.encodedUrl });
+      if (!encodedUrl) {
+        res.status(404).send({ message: "Orginal url not found!" });
+      } else {
+     // increase the collection stats by 1
+        encodedUrl.stats++;
+
+    // Update the collection stats
+        encodedUrl.save();
+        // after goting to the encoded url on the website redirect us to the dedcode url
+        res.redirect(`${encodedUrl.decodedUrl}`);
+      }
+    } catch (error) {
+      res.status(500).send({ message: "Something went wrong!" });
+    }
+  };
+  
--- server/src/routes/urlRoutes.ts
+++ server/src/routes/urlRoutes.ts
@@ -1,11 +1,14 @@
 import express from "express";
-import { decodeUrl, encodeUrl } from "../controllers/urlController";
+import { decodeUrl, encodeUrl, getAllEncodedUrls, getUrlStats, goToEncodedUrlLink } from "../controllers/urlController";
 
 
 const router = express.Router();
 
 router.post("/encode", encodeUrl);
-router.get("/decode", decodeUrl);
+router.post("/decode", decodeUrl);
+router.get("/statistic/:encodedUrl", getUrlStats);
+router.get("/allUrls", getAllEncodedUrls);
+router.get("/encodedUrl/:encodedUrl", goToEncodedUrlLink);
 
 
 export default router;
